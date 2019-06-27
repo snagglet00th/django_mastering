ccaca691631784af1e2c23fab4ee545a4aef089f
snaggletooth <snagglet00th@yandex.ru>
2019-06-27 23:13:40 +0500

    improve:blog.admin.py # change admin.site.register to class(admin.ModelAdmin)

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 
-from blog.models import Post
+from .models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class AdminPost(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')  # боковой толбар с фильтрами
+    search_fields = ('title', 'body')  # текстовое поле для поиска по указанным атрибутам
+    prepopulated_fields = {'slug': ('title',)}  # с вводом title, поле slug заполняется автоматом
+    raw_id_fields = ('author',)
+    date_hierarchy = "publish"  # появляется пагинация по датам
+    ordering = ('status', 'publish')
+
